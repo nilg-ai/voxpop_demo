4cc3136757ddcdebbb74c89346c29edb24837c76
Diogo Ribeiro <diogo.ribeiro@evooq.ch>
2023-02-07 23:39:07 +0000

    add map loading

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -6,14 +6,14 @@ import 'leaflet/dist/leaflet.css';
 import L, { LatLng } from 'leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster'
 import { IMarker } from '../../interfaces/marker';
+import { Spinner } from 'flowbite-react';
 
-function LocationMarkers(props: any) {
+function LocationMarkers({ onSelectMarker, setLoading, isLoading }: { onSelectMarker: (marker: IMarker) => void, setLoading: (state: boolean) => void, isLoading: boolean }) {
   const customIcon = new L.Icon({
     iconUrl: marker
   })
 
   const [markers, setMarkers] = useState<IMarker[]>([]);
-  const [isLoading, setLoading] = useState<boolean>(false);
 
   const getData = () => {
     setLoading(true);
@@ -32,10 +32,10 @@ function LocationMarkers(props: any) {
   }
 
   useEffect(() => {
-    if(markers.length === 0 && !isLoading) {
+    if (markers.length === 0 && !isLoading) {
       getData();
     }
-  }, [markers.length, isLoading]);
+  });
 
   return (
     <MarkerClusterGroup
@@ -43,7 +43,7 @@ function LocationMarkers(props: any) {
     >
       {markers.map((marker, i) => <Marker key={i} position={new LatLng(marker.lat, marker.long)} icon={customIcon} eventHandlers={{
         click: () => {
-          props.onSelectMarker(marker)
+          onSelectMarker(marker)
         },
       }}>
       </Marker>)}
@@ -51,16 +51,20 @@ function LocationMarkers(props: any) {
   );
 }
 
-function Map(props: any) {
-  return (<>
-    <MapContainer className='h-screen' center={[38.75, -9.15]} zoom={14}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <LocationMarkers onSelectMarker={props.onSelectMarker}></LocationMarkers>
-    </MapContainer>
-  </>)
+function Map({ onSelectMarker }: { onSelectMarker: (marker: IMarker) => void }) {
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  return (
+    <div className='relative'>
+      {isLoading ? <div className='absolute h-full w-full z-20 bg-slate-400 opacity-70 flex justify-center items-center'><Spinner size="xl" /></div> : <></>}
+      <MapContainer className='relative h-screen z-10' center={[38.75, -9.15]} zoom={14}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LocationMarkers onSelectMarker={onSelectMarker} setLoading={setLoading} isLoading={isLoading}></LocationMarkers>
+      </MapContainer>
+    </ div>)
 }
 
 export default Map;
