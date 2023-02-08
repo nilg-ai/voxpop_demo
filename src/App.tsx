ad861ed0efbc77709c387ff0f842dab3238b4373
Diogo Ribeiro <diogo.ribeiro@evooq.ch>
2023-02-08 16:48:30 +0000

    add cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/header/Header';
 import Map from './components/map/Map';
 import MarkerDetail from './components/marker-detail/MarkerDetail';
 import { IMarker } from './interfaces/IMarker';
+import { v4 as uuid } from 'uuid';
 
 function App() {
-  const [isShow, setIsShow] = React.useState(false);
+  const [isShowingDetail, showDetail] = React.useState(false);
   const [selectedMarker, setSelectedMarker] = useState<IMarker | undefined>(undefined);
 
   function closeDetails() {
-    setIsShow(false);
+    showDetail(false);
   }
 
   function onSelectMarker(marker: IMarker) {
-    setIsShow(true);
+    showDetail(true);
     setSelectedMarker(marker);
   }
+
+  useEffect(() => {
+    if(!document.cookie) {
+      document.cookie = uuid();
+    }
+  });
 
   return (
     <>
       <Header></Header>
       <div className="relative">
         <div className="absolute h-full sm:w-full md:w-1/3 lg:w-1/4 w-full z-20">
-          {isShow ? (
+          {isShowingDetail ? (
             <MarkerDetail selectedMarker={selectedMarker} onCloseDetails={closeDetails}></MarkerDetail>
           ) : (
             <></>
