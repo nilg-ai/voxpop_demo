a4d7454952fb426bbaf20d98414ee9d88ccc145f
Diogo Ribeiro <diogo.ribeiro@evooq.ch>
2023-02-15 11:23:50 +0000

    add cookie links

--- src/components/cookie-banner/CookieBanner.tsx
+++ src/components/cookie-banner/CookieBanner.tsx
@@ -12,10 +12,16 @@ function CookieModal({ show, onDismiss, onAccept }: { show: boolean, onDismiss: 
         Privacy Overview
       </Modal.Header>
       <Modal.Body>
-        <div className="space-y-6">
-          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+        <div className="space-y-6 text-base leading-relaxed text-gray-500">
+          <p>
             This website uses cookies to improve your experience while you navigate through the website. Out of these, the cookies that are categorized as necessary are stored on your browser as they are essential for the working of basic functionalities of the website. We also use third-party cookies that help us analyze and understand how you use this website. These cookies will be stored in your browser only with your consent. You also have the option to opt-out of these cookies. But opting out of some of these cookies may affect your browsing experience.
           </p>
+          <p>Check our cookie policy, terms, and privacy policy</p>
+          <ul>
+            <li><a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://nilg.ai/terms-of-service/" target="_blank" rel="noreferrer">https://nilg.ai/terms-of-service/</a></li>
+            <li><a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://nilg.ai/privacy-policy/" target="_blank" rel="noreferrer">https://nilg.ai/privacy-policy/</a></li>
+            <li><a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://nilg.ai/cookie-policy/" target="_blank" rel="noreferrer">https://nilg.ai/cookie-policy/</a></li>
+          </ul>
         </div>
       </Modal.Body>
       <Modal.Footer>
