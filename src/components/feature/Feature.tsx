8b51783753ad625f498b5796d815e570a84d01f0
Diogo Ribeiro <diogo.ribeiro@evooq.ch>
2023-05-23 12:27:45 +0100

    add dynamic colors

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,20 +1,25 @@
-function Feature({label, prob, icon}: {label: string, prob: number, icon: string}) {
-
-  const getColorByProb = (prob: number) => {
-    if(prob > 0.66) return 'text-nilg-green'
-    if(prob > 0.33) return 'text-nilg-yellow'
-    return 'text-nilg-orange';
-  }
-
-  return (
-    <div className="flex flex-col text-center items-center mb-3">
-      <div className="border-solid border-slate-500 rounded-full">
-        <img src={icon} alt={label}/>
-      </div>
-      <div className="text-xs font-medium">{label}</div>
-      <div className={"text-xs font-semibold " + getColorByProb(prob)}>{prob}</div>
-    </div>
-  )
+function Feature({
+    label,
+    prob,
+    icon,
+    color,
+}: {
+    label: string
+    prob: number
+    icon: string
+    color: string
+}) {
+    return (
+        <div className="mb-3 flex flex-col items-center text-center">
+            <div className="rounded-full border-solid border-slate-500">
+                <img src={icon} alt={label} />
+            </div>
+            <div className="text-xs font-medium">{label}</div>
+            <div style={{ color }} className={`text-xs font-semibold`}>
+                {prob}
+            </div>
+        </div>
+    )
 }
 
-export default Feature;
+export default Feature
--- src/interfaces/IMarker.ts
+++ src/interfaces/IMarker.ts
@@ -1,14 +1,15 @@
 export interface IMarker {
-  lat: number;
-  long: number;
-  likes?: number;
-  dislikes?: number;
-  address?: string;
-  directionsUrl?: string;
-  features?: {
-    label: string;
-    prob: number;
-    icon: string;
-  }[];
-  color?: string;
+    lat: number
+    long: number
+    likes?: number
+    dislikes?: number
+    address?: string
+    directionsUrl?: string
+    features?: {
+        label: string
+        prob: number
+        icon: string
+        color: string
+    }[]
+    color?: string
 }
