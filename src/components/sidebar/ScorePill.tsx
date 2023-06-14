f6b38ea262b0da775a4b21966d4fa289dc17f9a1
Diogo Ribeiro <diogo.ribeiro@evooq.ch>
2023-06-14 15:48:37 +0100

    fix color

--- src/components/sidebar/ScorePill.tsx
+++ src/components/sidebar/ScorePill.tsx
@@ -1,13 +1,15 @@
 import { AiFillStar } from 'react-icons/ai'
 import { IRoute } from '../../interfaces/IRoute'
+import { useEffect } from 'react'
 
 function ScorePill({ route }: { route: IRoute }) {
+    useEffect(() => {
+        console.log(route)
+    }, [route])
     return (
         <div
-            className={
-                'justify-content flex items-center gap-1 rounded-md bg-nilg-green px-2 py-1 text-sm font-semibold text-white ' +
-                `bg-${route.color}`
-            }
+            className="justify-content flex items-center gap-1 rounded-md px-2 py-1 text-sm font-semibold text-white"
+            style={{ backgroundColor: route.color }}
         >
             <AiFillStar /> {route.average_accessibility}
         </div>
